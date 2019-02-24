c8df8130e418f3f577e759278218031d2c6b7c7d
shubhamanand43 <suvm43@gmail.com>
2019-02-24 09:33:21 +0530

    Complete Stage 1

--- record.py
+++ record.py
@@ -1,63 +1,64 @@
 
 # Read(r) ++
 # Write(w) ++
-# Update(u) ++
+# alter(a) ++
 # Search(s) ++
 # delete(d) ++
 
 class RecordManage():
 
+    # constrctor for initializing file every time new object is made to use some of the Fuctions..
     def __init__(self,fn):
         fc = open(file=fn, mode='r')
         self.st = fc.read()
+        # get all the file content in a list.
         self.ls = self.st.split('|')
         fc.close()
         self.mls = []
         ils = []
-
+        # Make a Matrix of DATA Arranged of Above list as a different 2D List.
         for i in range(0, int((len(self.ls)-1)/4)):
             for j in range(i*4+1, (i+1)*4+1):
                 ils.append(self.ls[j])
             self.mls.append(ils.copy())
             ils.clear()
-        #print(self.mls)
 
     def read(self,ls):
-        s = int(input("Enter Start line (Can be ZERO, if e = 0 it will give Single Line) : "))
-        e = int(input("Enter End line for START to END result or For Single Line put '0' : "))
-        #print(ls)
+        """ # Read as desired line or desired lines Consecutively Adjecent. """
+        s = int(input("Enter Start line (Can be ZERO, if e = 0 it will give Single Line) : ")) # take START index of Line
+        e = int(input("Enter End line for START to END result or For Single Line put '0' : ")) # take END index as Exclusive of Line or put '0' to get only 1 record as indexed START.
+
         if e == 0:
-            lt = ls[s*4:(s+1)*4+1].copy()
+            lt = ls[s*4:(s+1)*4+1].copy() # Copy() Makes Copy of data in a different Mamory location Don't Point to the same.
         else:
             lt = ls[s*4:e*4+1].copy()
-
-        #print(lt)
+        # Returns the list Croped to get the desired Elements.
         return " | ".join(lt)
 
-
     def write(self,fn):
+        """ # Writes Record(s) using user inputs and append to the Working file. """
         n = int(input("Enter How Many Record you want to store : "))
         for i in range(0,n):
             name = input("Enter Name : ")
             usn = input("Enter USN : ")
             marks = input("Enter Marks : ")
             print("\n")
-
+            # Open as append mode
             f = open(file=fn ,mode='a')
             f.seek(0,2)
+            # Makes a record  as a String to Fit in the file.
             st = "|" + name + "|" + usn + "|" + marks + "|"
             rs = st  + "\n"
             f.write(rs)
             f.close()
 
     def search(self,ls,mls):
+        """ # Searchs Record Using Key input As USN or Name. """
         sb = input("Enter 'n' for SEARCH BY NAME or 'u' for SEARCH BY USN : ")
         ki = input("Enter keyword to FIND : ")
         l = ls
         m = mls
-        #print(l)
-        #print(m)
-
+        # Func to find out the Where Element Located.
         def colS(ls, mls, col, ki):
             serc = True if ki in [i[col] for i in mls] else False
             if serc == True:
@@ -67,7 +68,7 @@ class RecordManage():
             else:
                 print("NOT FOUND")
                 return -1
-
+        # if-else for choosing the SEARCH BY NAME or SEARCH BY USN
         if sb == 'n':
             ch = colS(l,m,0,ki)
         elif sb == 'u':
@@ -75,10 +76,11 @@ class RecordManage():
 
         if ch == -1: pass
         else:
-            #print(ch)
+            # Prints list of FOUND Data.
             print(mls[ch][:3])
 
     def alter(self,ls,mls,fn):
+        """ # alter data ie change USN or Name at a time or Change Marks giving USN to Search data. """
             gm = input("Enter 'u' for Change USN or NAME  or 'm' for change marks : ")
             if gm == "u" :
                 kin = input("Enter Name or Usn (Give USN to be SPECIFIC) to SEARCH it : ")
@@ -89,13 +91,16 @@ class RecordManage():
 
             serr = True if kin in ls else False
             if serr == True:
+                # Assigning the or changeing the values according.
                 if gm == 'u':
                     ls[ls.index(kin)] = kch
                 elif gm == 'm':
                     ls[ls.index(kin)+1] = min
+                # it will clear all data in the file
                 f = open(file=fn, mode='w')
                 f.write("")
                 f.close()
+                # Now again rewrite the Modified data.
                 for i in range(0,len(mls)):
                     name = ls[i*4+1]
                     usn = ls[i*4+2]
@@ -115,19 +120,15 @@ class RecordManage():
         if serr == True:
             l = ls.index(kin)
             del ls[l-1:l+3]
-            #lc = ls.copy()
-            #print(ls)
-            #print(mls)
-            #m = len(mls)
+            # it will clear all data in the file
             f = open(file=fn,mode='w')
             f.write("")
             f.close()
-            #print(lc)
+            # Now again rewrite the Modified data.
             for i in range(0,len(mls)-1):
                 name = ls[i*4+1]
                 usn = ls[i*4+2]
                 marks = ls[i*4+3]
-                #print(name)
                 st = "|" + name + "|" + usn + "|" + marks + "|"
                 rs = st + "\n"
                 f = open(file=fn, mode='a')
@@ -135,6 +136,7 @@ class RecordManage():
                 f.close()
 
 
+# Ask to use NEW file or Previous ONE.
 askf = input("Enter 'nf' MAKE NEW FILE or 'pf' for USE PREVIOUS FILE : ")
 fn = input("Enter file Name : ")
 if askf == "nf":
@@ -143,6 +145,7 @@ if askf == "nf":
 elif askf == "pf" :
     pass
 
+# All Above Functions all organised to work properly.
 run = True
 while run == True:
     ch = input("Enter 'r' for READ, 'w' for WRITE, 's' for SEARCH, 'u' for UPDATE and 'q' to QUIT : ")
