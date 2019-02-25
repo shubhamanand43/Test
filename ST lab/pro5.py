acb86e97d78dbfeea06fb0e9d8f2768599ad6f06
Shubham Anand <42351193+shubhamanand43@users.noreply.github.com>
2019-02-25 18:12:16 +0530

    Delete pro5.py

--- ST lab/pro5.py
+++
@@ -1,7 +0,0 @@
-"""
-5. Design, develop, code and run the program in any suitable language to solve
-the commission problem. Analyze it from the perspective of equivalence class
-testing, derive different test cases, execute these test cases and discuss the
-test results.
-
-"""
